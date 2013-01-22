cc37969b42bb58b676c1bb6e34c88ad14c6b71c4
Philipp Schuster <pschuster@uni-koblenz.de>
2013-01-22 17:19:11 +0100

    hardcoded test

--- Web/DatomicHTTP.hs
+++ Web/DatomicHTTP.hs
@@ -1,6 +1,17 @@
 module DatomicHTTP where
 
+import Network.HTTP (simpleHTTP,getResponseBody)
+import Network.HTTP.Base (Request(Request),RequestMethod(GET))
+import Network.HTTP.Headers (Header(Header),HeaderName(HdrAccept))
+import Network.URI (parseURI)
+import Data.Maybe (fromJust)
 
 
+main :: IO ()
+main = do
+    let uri = fromJust (parseURI "http://127.0.0.1:9834/api/query?q=%5B%3Afind+%3Fe+%3Fv+%3Ain+%24+%3Awhere+%5B%3Fe+%3Adb%2Fdoc+%3Fv%5D%5D&args=%5B%7B%3Adb%2Falias+%22whatup%2Ftst%22%7D%5D&offset=&limit=")
+        hdr = Header HdrAccept "application/edn"
+        req = Request uri GET [hdr] ""
+    simpleHTTP req >>= getResponseBody >>= print
 
 

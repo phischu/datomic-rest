86a87469b9a7bb991228687f2d953b76ad234123
Philipp Schuster <pschuster@uni-koblenz.de>
2013-02-27 16:27:08 +0100

    Well some exploration of the transaction dsl design space

--- Web/DatomicDSL.hs
+++ Web/DatomicDSL.hs
@@ -1,6 +1,70 @@
 module DatomicDSL where
 
+import Control.Monad.Free
 
+test :: Transaction ()
+test = do
+    bob_id   <- newTempId
+    alice_id <- newTempId
+    multiAdd (entityTempId bob_id)   [keywordNS "person" "name"   |~> string      "Bob",
+                                      keywordNS "person" "spouse" |~> valueTempId alice_id]
+    multiAdd (entityTempId alice_id) [keywordNS "person" "name"   |~> string      "Alice",
+                                      keywordNS "person" "spouse" |~> valueTempId bob_id]
+    add (entityTempId bob_id) (keywordNS "person" "height") (floating 1.87)
+
+type Transaction = Free TransactionF
+
+data TransactionF a = TransactionF
+
+data AttributeValuePair = AttributeValuePair
+
+data TempId = TempId
+
+instance Functor TransactionF where
+    fmap _ _ = TransactionF
+
+data Attribute = Attribute
+
+data Value = Value
+
+keywordNS :: String -> String -> Attribute
+keywordNS = undefined
+
+string :: String -> Value
+string = undefined
+
+entityTempId :: TempId -> Entity
+entityTempId = undefined
+
+valueTempId :: TempId -> Value
+valueTempId = undefined
+
+(|~>) :: Attribute -> Value -> AttributeValuePair
+(|~>) = undefined
+
+multiAdd :: Entity -> [AttributeValuePair] -> Transaction ()
+multiAdd = undefined
+
+add :: Entity -> Attribute -> Value -> Transaction ()
+add = undefined
+
+data Entity = Entity
+
+floating :: Double -> Value
+floating = undefined
+
+newTempId :: Transaction TempId
+newTempId = undefined
+
+{-
+
+data TransactionF a = NewTempID (TempID -> a)
+                    | Add EAV a
+                    | Retract EAV a
+
+type TempID = Int
+
+data EAV = EAV EntityId Attribute Value
 
 data Transaction = Transaction [TransactionStatement]
 
@@ -30,4 +94,4 @@ data NegativeInt = NegativeInt Int
 data Partition = Partition
 
 data Identifier = Ident
-
+-}

c17c4cf56d9815f0f73d9369d416e596b140d0ba
Philipp Schuster <pschuster@uni-koblenz.de>
2013-03-09 17:44:49 +0100

    optional attributes for schema creation

--- Web/Datomic/SchemaDSL.hs
+++ Web/Datomic/SchemaDSL.hs
@@ -3,21 +3,36 @@ module Web.Datomic.SchemaDSL where
 
 import Web.Datomic.TransactionDSL
 
-import Data.Text
+import Data.Text hiding (concat)
 
 import Control.Monad (forM)
 
 --ATTRIBUTES
 
-schema :: Keyword -> ValueType -> Cardinality -> [OptionalAttribute] -> Transaction TempId
-schema ident valuetype cardinality [] = do
+schema :: Keyword -> ValueType -> Cardinality -> Transaction TempId
+schema ident valuetype cardinality = do
     tempid <- newTempId (key "db.part" "db")
     multiAdd tempid [key "db" "ident"             |~> ident,
                      key "db" "valueType"         |~> valueTypeKeyword valuetype,
                      key "db" "cardinality"       |~> cardinalityKeyword cardinality,
                      key "db.install" "attribute" |<~ key "db.part" "db"]
     return tempid
-schema _ _ _ _ = undefined
+
+schemaEx :: Keyword -> ValueType -> Cardinality -> OptionalAttributes -> Transaction TempId
+schemaEx ident valuetype cardinality optionalAttributes = do
+    tempid <- schema ident valuetype cardinality
+    multiAdd tempid (interpretOptionalAttributes optionalAttributes)
+    return tempid
+
+interpretOptionalAttributes :: OptionalAttributes -> [AttributeValue]
+interpretOptionalAttributes optionalAttributes = concat $ [
+    maybe [] (\v -> [key "db" "doc" |~> str v]) (_doc oa),
+    maybe [] (\v -> [key "db" "unique" |~> interpretUnique v]) (_unique oa),
+    maybe [] (\v -> [key "db" "index" |~> v]) (_index oa),
+    maybe [] (\v -> [key "db" "fulltext" |~> v]) (_fulltext oa),
+    maybe [] (\v -> [key "db" "isComponent" |~> v]) (_isComponent oa),
+    maybe [] (\v -> [key "db" "noHistory" |~> v]) (_noHistory oa)] where
+        oa = optionalAttributes
 
 data ValueType = TypeRef
                | TypeKeyword
@@ -55,12 +70,30 @@ cardinalityKeyword :: Cardinality -> Keyword
 cardinalityKeyword One = key "db.cardinality" "one"
 cardinalityKeyword Many = key "db.cardinality" "many"
 
-data OptionalAttribute = Doc
-                       | Unique
-                       | Index
-                       | Fulltext
-                       | IsComponent
-                       | NoHistory
+data OptionalAttribute = Doc Text
+                       | Unique Unique
+                       | Index Bool
+                       | Fulltext Bool
+                       | IsComponent Bool
+                       | NoHistory Bool
+
+data Unique = UniqueValue
+            | UniqueIdentity
+
+interpretUnique :: Unique -> Keyword
+interpretUnique UniqueValue = key "db.unique" "value"
+interpretUnique UniqueIdentity = key "db.unique" "identity"
+
+data OptionalAttributes = OptionalAttributes {
+    _doc :: Maybe Text,
+    _unique :: Maybe Unique,
+    _index :: Maybe Bool,
+    _fulltext :: Maybe Bool,
+    _isComponent :: Maybe Bool,
+    _noHistory :: Maybe Bool}
+
+defaultOptionalAttributes :: OptionalAttributes
+defaultOptionalAttributes = OptionalAttributes Nothing Nothing Nothing Nothing Nothing Nothing
 
 --PARTITIONS
 
@@ -75,7 +108,7 @@ partition partitionname = do
 
 enum :: Keyword -> Keyword -> [Keyword] -> Transaction TempId
 enum part enumname enumvalues = do
-    enumid <- schema enumname TypeRef One []
+    enumid <- schema enumname TypeRef One
     forM enumvalues $ (\enumvalue -> do
         enumvalueid <- newTempId part
         add enumvalueid (key "db" "ident") enumvalue)
